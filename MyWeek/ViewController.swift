be0eda5cecbe2f1ac64f05e75ba5773c2383865f
weiting <a0188000@gmail.com>
2020-04-24 09:35:33 +0800

    Initial Commit

---
+++ MyWeek/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeek
+//
+//  Created by EVERTRUST on 2020/4/24.
+//  Copyright © 2020 Shen Wei Ting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
